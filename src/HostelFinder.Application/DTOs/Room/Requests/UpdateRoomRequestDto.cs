52a550b6de6d62e34c6fe776f3c4ae42cb0eede6
vietthaist01@gmail.com <vietthaist01@gmail.com>
2024-09-28 12:14:08 +0700

    add dto

---
+++ src/HostelFinder.Application/DTOs/Room/Requests/UpdateRoomRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using HostelFinder.Domain.Enums;
+
+namespace HostelFinder.Application.DTOs.Room.Requests;
+
+public class UpdateRoomRequestDto
+{
+    public Guid HostelId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public virtual RoomType RoomType { get; set; } 
+    public decimal? Size { get; set; }
+    public decimal MonthlyRentCost { get; set; }
+    public bool IsAvailable { get; set; } = true;
+    public DateTime DateAvailable { get; set; }
+
+}
