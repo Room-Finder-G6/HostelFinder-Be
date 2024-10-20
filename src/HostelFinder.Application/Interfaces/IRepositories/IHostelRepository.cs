b536e1c2b6a857940e51f4b04065cfb8cbef28c9
TungTran17 <tungtvhe163636@fpt.edu.vn>
2024-10-20 23:37:03 +0700

    Update-ResponseOfHosstel

--- src/HostelFinder.Application/Interfaces/IRepositories/IHostelRepository.cs
+++ src/HostelFinder.Application/Interfaces/IRepositories/IHostelRepository.cs
@@ -8,4 +8,6 @@ public interface IHostelRepository : IBaseGenericRepository<Hostel>
     Task<bool> CheckDuplicateHostelAsync(string hostelName, string province, string district, string commune, string detailAddress);
     Task<IEnumerable<Hostel>> GetHostelsByLandlordIdAsync(Guid landlordId);
     Task<Hostel> GetHostelWithReviewsByPostIdAsync(Guid postId);
+    Task<Hostel> GetHostelByIdAsync(Guid postId);
+
 }
