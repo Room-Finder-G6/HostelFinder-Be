28e6767a83250ffdcccfbb110afcc6a972d13c01
TungTV17 <trantungmr11@gmail.com>
2024-10-27 15:34:42 +0700

    Validation AddRoom

--- src/HostelFinder.Infrastructure/Repositories/RoomRepository.cs
+++ src/HostelFinder.Infrastructure/Repositories/RoomRepository.cs
@@ -2,6 +2,7 @@
 using HostelFinder.Domain.Entities;
 using HostelFinder.Infrastructure.Common;
 using HostelFinder.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace HostelFinder.Infrastructure.Repositories
 {
@@ -9,6 +10,11 @@ namespace HostelFinder.Infrastructure.Repositories
     {
         public RoomRepository(HostelFinderDbContext dbContext) : base(dbContext)
         {
+        }
+
+        public async Task<bool> RoomExistsAsync(string roomName, Guid hostelId)
+        {
+            return await _dbContext.Rooms.AnyAsync(r => r.RoomName == roomName && r.HostelId == hostelId);
         }
     }
 }
