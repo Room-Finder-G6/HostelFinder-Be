eadb6b668fc8199d114d0a0db5ba50ffc32c8cfe
vietthaist01@gmail.com <vietthaist01@gmail.com>
2024-11-08 00:08:13 +0700

    done

--- src/HostelFinder.Application/DTOs/Hostel/Responses/ListHostelResponseDto.cs
+++ src/HostelFinder.Application/DTOs/Hostel/Responses/ListHostelResponseDto.cs
@@ -1,14 +1,17 @@
-﻿using HostelFinder.Application.DTOs.Image.Responses;
+﻿using HostelFinder.Application.DTOs.Address;
+using HostelFinder.Application.DTOs.Image.Responses;
 
 namespace HostelFinder.Application.DTOs.Hostel.Responses
 {
     public class ListHostelResponseDto
     {
+        public Guid Id { get; set; }
         public string LandlordUserName { get; set; }
         public string HostelName { get; set; }
-        public string? Description { get; set; }
+        public AddressDto Address { get; set; }
         public float? Size { get; set; }
         public int NumberOfRooms { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
         public List<ImageResponseDto> Image { get; set; }
     }
 }
