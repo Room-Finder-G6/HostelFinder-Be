953caa1ef6f230c64471a0f03a81ed414cfef8bb
Hiếu Mai Trọng <hieumthe163841@fpt.edu.vn>
2024-11-17 21:36:27 +0700

    feature(add contact): add config service and repository entities

---
+++ src/HostelFinder.Infrastructure/Repositories/VehicleRepository.cs
@@ -0,0 +1,14 @@
+﻿using HostelFinder.Application.Interfaces.IRepositories;
+using HostelFinder.Domain.Entities;
+using HostelFinder.Infrastructure.Common;
+using HostelFinder.Infrastructure.Context;
+
+namespace HostelFinder.Infrastructure.Repositories
+{
+    public class VehicleRepository : BaseGenericRepository<Vehicle>, IVehicleRepository
+    {
+        public VehicleRepository(HostelFinderDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
