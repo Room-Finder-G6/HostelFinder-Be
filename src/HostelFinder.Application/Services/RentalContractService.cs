dc9676b7c84bd9e3660e1df81b750bdd7c617cb4
Hiếu Mai Trọng <hieumthe163841@fpt.edu.vn>
2024-11-20 10:32:55 +0700

    feature(retraContract) : add meter reading service in retranl contract

--- src/HostelFinder.Application/Services/RentalContractService.cs
+++ src/HostelFinder.Application/Services/RentalContractService.cs
@@ -14,18 +14,21 @@ namespace HostelFinder.Application.Services
         private readonly IRoomTenancyRepository _roomTenancyRepository;
         private readonly ITenantService _tenantService;
         private readonly IMapper _mapper;
+        private readonly IMeterReadingService _meterReadingService;
         public RentalContractService(
             IRentalContractRepository rentalContractRepository,
             IRoomRepository roomRepository,
             IRoomTenancyRepository roomTenancyRepository,
             ITenantService tenantService,
-            IMapper mapper)
+            IMapper mapper,
+            IMeterReadingService meterReadingService)
         {
             _rentalContractRepository = rentalContractRepository;
             _roomRepository = roomRepository;
             _roomTenancyRepository = roomTenancyRepository;
             _tenantService = tenantService;
             _mapper = mapper;
+            _meterReadingService = meterReadingService;
         }
         public async Task<Response<string>> CreateRentalContractAsync(AddRentalContractDto request)
         {
@@ -43,6 +46,7 @@ namespace HostelFinder.Application.Services
                     throw new Exception("Phòng đã đạt số lượng người thuê tối đa");
                 }
 
+                // tạo hợp đồng
                 var rentalContract = new RentalContract
                 {
                     TenantId = tenantCreated.Id,
@@ -57,8 +61,9 @@ namespace HostelFinder.Application.Services
                 };
 
                 await _rentalContractRepository.AddAsync(rentalContract);
-                // tạo mới roomTenacy cho người thuê chính
 
+
+                // tạo mới roomTenacy cho người thuê chính
                 var roomTenacy = new RoomTenancy
                 {
                     TenantId = tenantCreated.Id,
@@ -69,11 +74,22 @@ namespace HostelFinder.Application.Services
 
                 await _roomTenancyRepository.AddAsync(roomTenacy);
 
+
+                // kiểm tra số lượng trong phòng
                 if(currentTenantsCount + 1 >= room.MaxRenters)
                 {
                     room.IsAvailable = false;
                    await _roomRepository.UpdateAsync(room);
                 }
+
+                // ghi số dịch vụ tại thời điểm hợp đồng
+                //await _meterReadingService.AddMeterReadingAsync(room.Id, request.ServiceId, request.Reading, DateTime.Now.Month, DateTime.Now.Year);
+                var listMeterReaderingSevice = request.AddMeterReadingServiceDto;
+                foreach(var readingService in listMeterReaderingSevice)
+                {
+                    await _meterReadingService.AddMeterReadingAsync(room.Id, readingService.ServiceId, readingService.Reading, DateTime.Now.Month, DateTime.Now.Year);
+                }
+
                 return new Response<string> { Succeeded = true, Message = $"Tạo hợp đồng cho thuê thành công cho phòng {room.RoomName} với người thuê : {tenantCreated.FullName}" };
             }
             catch (Exception ex)
