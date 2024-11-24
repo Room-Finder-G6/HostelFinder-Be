2eb81b345ba5ad2a9546e4298520d18d6783181e
TungTV17 <trantungmr11@gmail.com>
2024-11-24 13:26:39 +0700

    API GetMembershipServicesForUserAsync

--- src/HostelFinder.Application/DTOs/MembershipService/Responses/PostingMemberShipServiceDto.cs
+++ src/HostelFinder.Application/DTOs/MembershipService/Responses/PostingMemberShipServiceDto.cs
@@ -5,5 +5,4 @@ public class PostingMemberShipServiceDto
     public Guid Id { get; set; }
     public string TypeOfPost { get; set; }
     public int NumberOfPostsRemaining { get; set; }
-    public int NumberOfPushTopRemaining { get; set; }
 }
