c662eb589f0f8279f73cc9ec536a25c77a995632
vietthaist01@gmail.com <vietthaist01@gmail.com>
2024-11-24 20:56:39 +0700

    modify membership text

--- src/HostelFinder.Application/DTOs/Post/Responses/ListPostsResponseDto.cs
+++ src/HostelFinder.Application/DTOs/Post/Responses/ListPostsResponseDto.cs
@@ -10,7 +10,7 @@ public class ListPostsResponseDto
     public AddressDto Address { get; set; }
     public decimal MonthlyRentCost { get; set; }
     public decimal Size { get; set; }
-    public string MemberShipName { get; set; }
+    public string MembershipTag { get; set; }
     public string FirstImage { get; set; }
     public DateTimeOffset CreatedOn { get; set; }
 }
