d381ffd1ff36ae81f2d35b8b408674e468ed1b56
TungTV17 <trantungmr11@gmail.com>
2024-11-30 17:07:05 +0700

    API-AddRoomate

--- src/HostelFinder.Domain/Entities/RoomTenancy.cs
+++ src/HostelFinder.Domain/Entities/RoomTenancy.cs
@@ -1,5 +1,4 @@
-﻿using RoomFinder.Domain.Common;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 using HostelFinder.Domain.Common;
 
 namespace HostelFinder.Domain.Entities
