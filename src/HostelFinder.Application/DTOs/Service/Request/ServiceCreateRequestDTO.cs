025be5219663bed85c90db2690a25cda19513f2a
TungTV17 <trantungmr11@gmail.com>
2024-12-03 00:54:08 +0700

    service cost

--- src/HostelFinder.Application/DTOs/Service/Request/ServiceCreateRequestDTO.cs
+++ src/HostelFinder.Application/DTOs/Service/Request/ServiceCreateRequestDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace HostelFinder.Application.DTOs.Service.Request
+﻿using HostelFinder.Domain.Enums;
+
+namespace HostelFinder.Application.DTOs.Service.Request
 {
     public class ServiceCreateRequestDTO
     {
         public string ServiceName { get; set; }
         public Guid HostelId { get; set; }
         public int Price { get; set; }
+        public ChargingMethod ChargingMethod { get; set; }
+
     }
 }
