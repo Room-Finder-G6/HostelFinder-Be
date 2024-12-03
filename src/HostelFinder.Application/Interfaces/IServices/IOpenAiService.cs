96c567520de105aead23fb339d9ad0c56c15a3d0
Hiếu Mai Trọng <hieumthe163841@fpt.edu.vn>
2024-12-03 17:29:09 +0700

    implement openAI

---
+++ src/HostelFinder.Application/Interfaces/IServices/IOpenAiService.cs
@@ -0,0 +1,9 @@
+﻿using HostelFinder.Application.DTOs.ChatAI.Request;
+using HostelFinder.Application.DTOs.ChatAI.Response;
+
+namespace HostelFinder.Application.Interfaces.IServices;
+
+public interface IOpenAiService
+{
+    Task<OpenAiChatResponse> GenerateAsync (OpenAiChatRequest request);
+}
