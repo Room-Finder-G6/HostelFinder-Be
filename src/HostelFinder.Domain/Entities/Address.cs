ff36d7ac874d28ec3e1e4c2ad8966382f43421e1
TungTV17 <trantungmr11@gmail.com>
2024-12-08 06:17:10 +0700

    update fix bug conflic

--- src/HostelFinder.Domain/Entities/Address.cs
+++ src/HostelFinder.Domain/Entities/Address.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using HostelFinder.Domain.Common;
 
 namespace HostelFinder.Domain.Entities;
@@ -7,12 +6,10 @@ namespace HostelFinder.Domain.Entities;
 public class Address : BaseEntity
 {
     public Guid? HostelId { get; set; }
-    public Guid? StoryId { get; set; }
     public string Province { get; set; }
     public string District { get; set; }
     public string Commune { get; set; }
     [MaxLength(255)]
     public string DetailAddress { get; set; }
     public virtual Hostel? Hostel { get; set; }
-    public virtual Story? Story { get; set; }
 }
