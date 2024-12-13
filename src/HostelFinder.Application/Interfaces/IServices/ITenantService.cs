767423a93e888bbffb1114832e55937e60b2e7d3
Hiếu Mai Trọng <hieumthe163841@fpt.edu.vn>
2024-12-13 18:02:18 +0700

    update tenant

--- src/HostelFinder.Application/Interfaces/IServices/ITenantService.cs
+++ src/HostelFinder.Application/Interfaces/IServices/ITenantService.cs
@@ -1,6 +1,7 @@
 ﻿using HostelFinder.Application.DTOs.RentalContract.Request;
 using HostelFinder.Application.DTOs.RentalContract.Response;
 using HostelFinder.Application.DTOs.Room.Responses;
+using HostelFinder.Application.DTOs.Tenancies.Requests;
 using HostelFinder.Application.DTOs.Tenancies.Responses;
 using HostelFinder.Application.Wrappers;
 
@@ -13,5 +14,9 @@ namespace HostelFinder.Application.Interfaces.IServices
         Task<Response<string>> AddRoommateAsync(AddRoommateDto request);
         Task<PagedResponse<List<InformationTenanciesResponseDto>>> GetAllTenantsByHostelAsync(Guid hostelId, string? roomName, int pageNumber, int pageSize, string? status);
         Task<Response<string>> MoveOutAsync(Guid tenantId, Guid roomId);
+        
+        Task<Response<string>> UpdateTenantAsync(UpdateTenantDto request); 
+        
+        Task<Response<TenantResponseDto>> GetTenantByIdAsync(Guid tenantId);
     }
 }
